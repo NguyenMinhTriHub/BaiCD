cc40d9a9fb128c288a324778e5177576618b1b38
NguyenMinhTriHub <mtri20051002@gmail.com>
2024-09-13 09:25:48 +0700

    Commit thu hai

---
+++ src/Th3_23710141_NguyenMinhTri/ListCDM.java
@@ -0,0 +1,74 @@
+package Th3_23710141_NguyenMinhTri;
+
+public class ListCDM {
+	public int count;
+	CDR[] ls;
+	public ListCDM(int n) {
+		ls=new CDR[n];
+		count=0;
+	}
+	public void tangKT() {
+		CDR[] tam=new CDR[ls.length*2];
+		System.arraycopy(ls,0,tam,0,count);
+		ls=tam;
+	}
+	public boolean themCDR(CDR cd) {
+		if(count==ls.length) {
+			tangKT();
+		}
+		for(int i=0;i<count;i++) {
+			if(ls[i].getMaCD().equalsIgnoreCase(cd.getMaCD())) {
+				return false;
+			}
+		}
+		ls[count]=cd;
+		count++;
+		return true;
+	}
+	public boolean xoa(String maXoa) {
+		int viTri=-1;
+		for(int i=0;i<count;i++) {
+			if(ls[i].getMaCD().equalsIgnoreCase(maXoa)) {
+				viTri=i;
+			}
+		}
+		if(viTri!=-1) {
+			for(int i=viTri;i<count-1;i++) {
+				ls[i]=ls[i+1];
+				count--;
+				return true;
+			}
+		}else {
+			return false;
+		}
+		return false;
+	}
+	public boolean capNhat(String ma,double gia) {
+		int viTri=-1;
+		for(int i=0;i<count;i++) {
+			if(ls[i].getMaCD().equalsIgnoreCase(ma)) {
+				viTri=i;
+			}
+		}
+		if(viTri!=-1) {
+			ls[viTri].setGia(gia);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public void sapXep() {
+		for(int i=0;i<count;i++) {
+			for(int j=i+1;j<count-1;j++) {
+				if(ls[i].getMaCD().compareToIgnoreCase(ls[j].getMaCD())>0){
+					CDR tam=ls[i];
+					ls[i]=ls[j];
+					ls[j]=tam;
+				}
+			}
+		}
+	}
+	public CDR[]getAll(){
+		return ls;
+	}
+}
